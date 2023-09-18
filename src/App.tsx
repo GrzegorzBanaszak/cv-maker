ce0ad2c8c735965dbbf58b7a969e348c200d257f
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2023-09-18 18:45:56 +0200

    Simple convert to pdf example

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import { useRef } from "react";
 import "./App.css";
+import jsPDF from "jspdf";
 
 function App() {
-  return <></>;
+  const templeRef = useRef<HTMLDivElement>(null);
+
+  const handleGeneretPdf = () => {
+    const doc = new jsPDF({
+      format: "a4",
+      unit: "px",
+    });
+
+    doc.setFont("Inter-Regular", "normal");
+
+    doc.html(templeRef.current!, {
+      async callback(doc) {
+        doc.save("dock");
+      },
+    });
+  };
+  return (
+    <>
+      <button onClick={handleGeneretPdf}>Save</button>
+      <div ref={templeRef}>
+        <h1 className="bg-slate-500">Hello pdf</h1>
+      </div>
+    </>
+  );
 }
 
 export default App;
