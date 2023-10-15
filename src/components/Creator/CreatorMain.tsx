149fbf7d19eee340bf454eabca6af082f6fcd3f0
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2023-10-15 11:41:54 +0200

    Creator - input with description

--- src/components/Creator/CreatorMain.tsx
+++ src/components/Creator/CreatorMain.tsx
@@ -1,11 +1,12 @@
 import AboutGroup from "./components/AboutGroup";
+import InputWithDescription from "./components/InputWithDescription";
 import ManyFildsGroup from "./components/ManyFildsGroup";
 import SidePagination from "./components/SidePagination";
 import SideSelect from "./components/SideSelect";
 
 const CreatorMain: React.FunctionComponent = () => {
   return (
-    <div className="w-1/2 bg-white px-7 pt-10 ">
+    <div className="w-1/2 bg-white px-7 py-10 overflow-y-scroll">
       <div className="flex justify-between">
         <SidePagination />
         <SideSelect />
@@ -13,6 +14,8 @@ const CreatorMain: React.FunctionComponent = () => {
       <div>
         <AboutGroup />
         <ManyFildsGroup />
+        <InputWithDescription />
+        <InputWithDescription />
       </div>
     </div>
   );
--- src/components/Creator/components/InputWithDescription.tsx
+++ src/components/Creator/components/InputWithDescription.tsx
@@ -2,7 +2,8 @@ import { FunctionComponent, useState } from "react";
 import RemoveIcon from "../../../icons/RemoveIcon";
 import { motion } from "framer-motion";
 import AddIcon from "../../../icons/AddIcon";
-const ManyFildsGroup: FunctionComponent = () => {
+
+const InputWithDescription: FunctionComponent = () => {
   const [isHover, setIsHover] = useState(false);
   return (
     <div>
@@ -14,14 +15,18 @@ const ManyFildsGroup: FunctionComponent = () => {
         </div>
       </h2>
       <div className="flex items-center mt-5">
-        <textarea className="block border-2 border-gray-200 w-full  rounded-l-md h-24 resize-none p-2 focus:outline-none " />
+        <div className="w-full">
+          <textarea className="block border-2 border-gray-200 w-full  rounded-tl-md h-20 resize-none p-2 focus:outline-none m-0" />
+          <textarea className="block border-2 border-gray-200 w-full  rounded-bl-md h-20 resize-none p-2 focus:outline-none m-0" />
+        </div>
+
         <motion.div
           whileHover={{
             width: "4rem",
           }}
           onMouseEnter={() => setIsHover(true)}
           onMouseLeave={() => setIsHover(false)}
-          className="bg-red-500 h-24 rounded-r-md cursor-pointer flex items-center justify-center w-6"
+          className="bg-red-500 h-40 rounded-r-md cursor-pointer flex items-center justify-center w-6"
         >
           {isHover && <RemoveIcon isAnimated={true} />}
         </motion.div>
@@ -30,4 +35,4 @@ const ManyFildsGroup: FunctionComponent = () => {
   );
 };
 
-export default ManyFildsGroup;
+export default InputWithDescription;
